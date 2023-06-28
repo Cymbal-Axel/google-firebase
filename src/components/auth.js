50ff4ff0128386c86fe95cb2494f4ac055d5b290
Axel <cymbalaxel@gmail.com>
2023-06-28 17:57:51 -0300

    se agrego un boton "sign in" con un onClick, se importo auth y {createuser...}, y una funcion todavia sin codigo que es la que se le va a pasar al onClick

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,16 @@
+import { auth } from '../config/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 export const Auth = () => {
+    const signIn = () => {
+
+    }
+
     return(
         <div>
             <input placeholder="Email..."></input>
             <input placeholder="Password..."></input>
+            <button onClick={signIn}>Sign in</button>
         </div>
     )
 }
