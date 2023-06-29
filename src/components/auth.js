4d4f843407b9f1ab8518c718f221dc0cc8d2fd93
Axel <cymbalaxel@gmail.com>
2023-06-29 19:06:44 -0300

    se utilizo la funcion importada de crear usuario en la funcion "signIn"

--- src/components/auth.js
+++ src/components/auth.js
@@ -6,15 +6,14 @@ export const Auth = () => {
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
 
-
-    const signIn = () => {
-
+    const signIn = async () => {
+        await createUserWithEmailAndPassword(auth, email, password);
     }
 
     return(
         <div>
             <input placeholder="Email..." onChange={(e) => setEmail(e.target.value)}></input>
-            <input placeholder="Password..." onChange={(e) => setPassword(e.target.value)}></input>
+            <input placeholder="Password..." type="password" onChange={(e) => setPassword(e.target.value)}></input>
             <button onClick={signIn}>Sign in</button>
         </div>
     )
