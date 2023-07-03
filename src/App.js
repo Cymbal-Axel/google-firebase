22e807a752912edbf0fb6e0791f7f08600318cb0
Axel <cymbalaxel@gmail.com>
2023-07-03 13:51:01 -0300

    se importo db, se creo un estado "movieList" y un useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import { Auth } from './components/auth';
+import { db } from './config/firebase';
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const getMovieList = () => {
+
+    }
+
+  }, [])
+
+
   return (
     <div className="App">
       <Auth />
