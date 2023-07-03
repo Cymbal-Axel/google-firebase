810fb9275881b2b8f4f736208127ed316b0d8fa8
Axel <cymbalaxel@gmail.com>
2023-07-03 14:16:41 -0300

    se creo una constante "moviesCollectionRef"que va a usar el metodo "collection" que fue importado con "getDocs" tambien

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { Auth } from './components/auth';
 import { db } from './config/firebase';
+import { getDocs, collection } from '@firebase/firestore';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
 
+  const moviesCollectionRef = collection(db, "movies")
+
   useEffect(()=>{
-    const getMovieList = () => {
+    const getMovieList = async () => {
+      try{
+      const data = await getDocs(moviesCollectionRef);
+      console.log(data)
+      } catch(err) {
+        console.error(err);
+      }
 
     }
-
+    getMovieList()
   }, [])
 
 
